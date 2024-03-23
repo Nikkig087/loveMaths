7dea7a9b870407c7d806c4624dd8e10e83296cf9
NikkiGetsee <123640851+Nikkig087@users.noreply.github.com>
2024-03-23 19:13:04 +0000

    Set up of structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+function runGame (){  /* we will need this */
+
+
+}
+
+function checkAnswer (){  /*we will need this */
+
+
+}
+
+function calculateCorrectAnswer (){
+
+}
+
+function incrementScore(){  /*could have this to see how many right answers the user had before it was wrong */
+
+}
+
+function incrementWrongAnswer (){
+
+}
+
+function displayAdditionQuestion (){/*could have this for image*/
+
+}
+
+
+function displaySubtractQuestion() {  /*this for our question*/
+
+}
+
+function displayMultiplyQuestion(){
+    
+}
