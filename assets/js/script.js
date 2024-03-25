97c1300d6bd6206137f4937754dcb0a8a6684262
NikkiGetsee <123640851+Nikkig087@users.noreply.github.com>
2024-03-25 21:42:06 +0000

    Add favicon, and final tidy up for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,13 +11,22 @@ document.addEventListener("DOMContentLoaded", function () {
     for (button of buttons) {
         button.addEventListener("click", function () {
             if (this.getAttribute("data-type") == "submit") {
-                  checkAnswer();          
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");          // else we set this to the gameType which will tell us what game type we are wanting to run
                 runGame(gameType);                     // we want to have it run the game
             }
         });
     }
+    // listen for the key down event ie when a key is pressed, if the event key is Enter key then run checkAnswer function
+    // Every event creates an object that has properties such as key below
+
+    document.getElementById("answer-box").addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+
+        }
+    });
     runGame("addition");
 
 });
@@ -28,6 +37,12 @@ document.addEventListener("DOMContentLoaded", function () {
  */
 function runGame(gameType) {  /* we will need this */
 
+    document.getElementById("answer-box").value = ""; //each time our runGame function is called the value of the answer box will be clear
+
+
+    document.getElementById("answer-box").focus();// puts cursor in box
+
+
     // create two random numbers in our code between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
@@ -35,16 +50,21 @@ function runGame(gameType) {  /* we will need this */
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
     } else if (gameType === "multiply") {
-        displayMultiplyQuestion(num1,num2);
+        displayMultiplyQuestion(num1, num2);
     } else if (gameType === "minus") {
-        displaySubtractQuestion(num1, num2)
+        displaySubtractQuestion(num1, num2);
 
-    } else {
+    } else if (gameType === "division") {
+        displayDivisonQuestion(num1, num2);
+
+    }
+    
+    else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game Type: ${gameType}. Aborting`;        //This will stop the game
     }
 
-    
+
 
 }
 
@@ -54,18 +74,18 @@ function runGame(gameType) {  /* we will need this */
 
 
 function checkAnswer() {  /*we will need this */
- let userAnswer =parseInt(document.getElementById("answer-box").value);
- let calculatedAnswer = calculateCorrectAnswer();
- let isCorrect = userAnswer === calculatedAnswer[0];
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
 
- if (isCorrect){
-    alert ("Hey you got it right")
-    incrementScore();
- } else {
-   alert( `Aw you answered ${userAnswer}.  The correct answer is ${calculatedAnswer[0]}`);
-   incrementWrongAnswer();
- }
- runGame(calculatedAnswer[1]); //this runs a second game
+    if (isCorrect) {
+        alert("Hey you got it right");
+        incrementScore();
+    } else {
+        alert(`Aw you answered ${userAnswer}.  The correct answer is ${calculatedAnswer[0]}`);
+        incrementWrongAnswer();
+    }
+    runGame(calculatedAnswer[1]); //this runs a second game
 }
 
 
@@ -83,10 +103,15 @@ function calculateCorrectAnswer() {
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
-        return [operand1 - operand2, "minus"];} 
-        else{
+        return [operand1 - operand2, "minus"];
+    }
+    else if (operator === "/") {
+        return  [Math.floor(operand1 / operand2), "division"];
+    }
+    else {
         alert(`Unimplemented operator ${operator}`);
-        throw `Unimplemented operator ${operator}`,"Aborting"; }
+        throw `Unimplemented operator ${operator}`, "Aborting";
+    }
 
 }
 
@@ -113,19 +138,29 @@ function displayAdditionQuestion(operand1, operand2) {/*could have this for imag
 }
 
 
-function displaySubtractQuestion(operand1,operand2) {  /*this for our question*/
-    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1:operand2;  
+function displaySubtractQuestion(operand1, operand2) {  /*this for our question*/
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
     // set it to the largest of the two, so if operand1 is bigger return that else if operand2 is bigger return that instead
 
 
-    document.getElementById("operand2").textContent = operand2 > operand1 ? operand1 : operand2; 
+    document.getElementById("operand2").textContent = operand2 > operand1 ? operand1 : operand2;
 
     document.getElementById("operator").textContent = "-";
 }
 
 
-function displayMultiplyQuestion(operand1,operand2) {
+function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById("operand1").textContent = operand1;
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "x";
+}
+
+
+function displayDivisonQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    // set it to the largest of the two, so if operand1 is bigger return that else if operand2 is bigger return that instead
+
+
+    document.getElementById("operand2").textContent = operand2 > operand1 ? operand1 : operand2;
+    document.getElementById("operator").textContent = "/";
 }
